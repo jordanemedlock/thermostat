1ab9023a302993ede00d9030b1328f72e75c0306
Jordan Medlock <jordanemedlock@gmail.com>
2020-01-07 20:01:18 -0800

    Laptop changes

--- background.py
+++ background.py
@@ -10,14 +10,7 @@ scheduler = sched.scheduler(time.time, time.sleep)
 def run_thermostat(scheduler):
 	print('doing stuff')
 
-	with open(data_file, 'rb') as fp:
-		thermostat = pickle.load(fp)
-
 	thermostat.run_thermostat()
-
-	with open(data_file, 'wb') as fp:
-		pickle.dump(thermostat, fp)
-
 
 	scheduler.enter(60, 1, run_thermostat, (scheduler,))
 
