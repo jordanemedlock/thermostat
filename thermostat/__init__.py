db8b8077fdafb6a0376254826f507cd9a6c66ce2
Jordan Medlock <jordanemedlock@gmail.com>
2020-01-12 15:49:08 -0800

    added separate project folder

---
+++ thermostat/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, g, request
+import os
+import json
+from thermostat import views
+import apscheduler.schedulers.background as aps
+
+def create_app(test_config=None):
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_mapping(
+		SECRET_KEY = 'dev'
+	)
+	app.config.from_envvar('APP_SETTINGS')
+
+	try:
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
+
+	scheduler = aps.BackgroundScheduler()
+	scheduler.start()
+
+	app.register_blueprint(views.control)
+	app.register_blueprint(views.ui)
+
+	views.activate_thermostat(scheduler)
+
+	return app
